2067aed8325068be8952d88bf05f90e511d777e9
jboehrb <101402396+Err404674@users.noreply.github.com>
2025-06-07 15:26:45 +0300

    Create List.lua

---
+++ Animations/List.lua
@@ -0,0 +1,9 @@
+--//ABDM:REMIX\\--
+local Anims = {
+  ['LmbCombo'] = 'rbxassetid://18951646068',
+  ['LmbCombo2'] = 'rbxassetid://18951648870',
+  ['LmbCombo3'] = 'rbxassetid://18951650763',
+  ['LmbCombo4'] = 'rbxassetid://18951653118',
+  ['SuplexSlam'] = 'rbxassetid://18951587811',
+  ['EgoBoost'] = 'rbxassetid://18951564615'
+}
